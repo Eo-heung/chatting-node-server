e428926950c67c6c244aeacd9716751e994a7bf8
parkseryu <pjh971212@gmail.com>
2023-08-10 12:34:04 +0900

    feat-69: 랜덤채팅 관련 로직 추가

--- src/server2.js
+++ src/server2.js
@@ -6,6 +6,7 @@ import cors from "cors";
 // variable
 const PORT = 5000;
 const app = express();
+const waitingUsers = [];
 
 // middleware
 app.use(cors());
@@ -30,6 +31,33 @@ const ioServer = SocketIO(httpServer, {
 });
 
 ioServer.on("connection", (socket) => {
+  socket.on("request_random_chat", () => {
+    console.log("request_random_chat");
+    waitingUsers.push(socket);
+
+    if (waitingUsers.length >= 2) {
+      const userSocket2 = waitingUsers.pop();
+      const userSocket1 = waitingUsers.pop();
+
+      // 두 사용자를 같은 방으로 연결 (예: 'room-123')
+      const roomName = `random_chat-${userSocket1.id}-${userSocket2.id}`;
+      console.log(`roomName : ${roomName}`);
+      userSocket1.join(roomName);
+      userSocket2.join(roomName);
+      // 두 사용자에게 상대방의 정보를 보내주거나, 'matched' 이벤트를 보내줄 수 있습니다.
+      //   socket.to(roomName).emit("matched", roomName);
+      //   userSocket1.emit("matched", roomName);
+      userSocket1.emit("matched", roomName);
+      userSocket2.emit("matched", roomName);
+
+      userSocket1.emit("welcome");
+
+      //   userSocket1.to(roomName).emit("matched", userSocket2.id, roomName);
+      //   userSocket2.to(roomName).emit("matched", userSocket1.id, roomName);
+      //   userSocket2.emit("matched", roomName);
+    }
+  });
+
   socket.on("join_room", (roomName) => {
     console.log(`join_room ${roomName} `);
     socket.join(roomName);
@@ -47,7 +75,27 @@ ioServer.on("connection", (socket) => {
   });
 
   socket.on("ice", (ice, roomName) => {
+    console.log(`ice : ${ice}`);
     socket.to(roomName).emit("ice", ice);
+  });
+
+  socket.on("stop_random_chat", () => {
+    console.log("stop_random_chat");
+    const index = waitingUsers.indexOf(socket);
+    if (index !== -1) {
+      waitingUsers[index].disconnect();
+      waitingUsers.splice(index, 1);
+    }
+    socket.broadcast.emit("user-disconnected", socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+    const index = waitingUsers.indexOf(socket);
+    if (index !== -1) {
+      waitingUsers.splice(index, 1);
+      socket.broadcast.emit("user-disconnected", socket.id);
+    }
   });
 });
 
